baca5c36238ba3a7d40d6edd03bf10a5a9ae698d
Joel Ababio Dartey <joeldartey71@gmail.com>
2025-09-11 07:40:38 +0100

    first version

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+		<footer>
+			<div class="footer-container">
+				<div class="footer-content right">
+					<div class="footer-link">
+						<ul>
+							<li><a href="<?php echo $url?>" class="text-primary">FAQ's</a></li>
+							<li><a href="<?php echo $url?>" class="text-primary">Privacy Policy</a></li>
+							<li><a href="<?php echo $url?>" class="text-primary">Privacy Policy</a></li>
+							<li><a href="<?php echo $url?>" class="text-primary">Terms & condition</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer-content center">
+					<p class="text-primary">© 2023 Kilimanjaro Restaurants</p>
+				</div>
+				<div class="footer-content left">
+					<div class="social-media">
+						<a href="<?php echo $url?>"><i class="fa-brands fa-facebook"></i></a>
+						<a href="<?php echo $url?>"><i class="fa-brands fa-instagram"></i></a>
+						<a href="<?php echo $url?>"><i class="fa-brands fa-square-x-twitter"></i></a>
+						<a href="<?php echo $url?>"><i class="fa-regular fa-envelope"></i></a>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+	</div>
+	<script type="text/javascript">
+		function openNav(){
+			document.getElementById("nav-ul").style.display ="block"
+			document.getElementById("show-nav").style.display ="none"
+			document.getElementById("close-nav").style.display ="block"
+		}
+		
+			function closeNav(){
+			document.getElementById("nav-ul").style.display ="none"
+			document.getElementById("show-nav").style.display ="block"
+			document.getElementById("close-nav").style.display ="none"
+		}
+	</script>
+</body>
+</html>
